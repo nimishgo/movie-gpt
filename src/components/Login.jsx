e92dc58f8cf08f40fcabc5bbee6dbb510c4c9704
nimishgo <nimish.2711@gmail.com>
2024-06-06 09:23:44 +0530

    chore : added redux

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,16 @@
 import { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMsg, setErrorMsg] = useState(null);
+  const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
 
@@ -14,8 +20,39 @@ const Login = () => {
 
   const handleClickForm = () => {
     const message = checkValidData(email.current.value, password.current.value);
-    console.log(message);
     setErrorMsg(message);
+    if (message) return;
+    if (!isSignInForm) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          alert("User is registered" + user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMsg(errorCode + " " + errorMessage);
+        });
+    } else {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          alert(user.email + " signed up");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMsg(errorCode + " " + errorMessage);
+        });
+    }
   };
 
   return (
@@ -23,7 +60,7 @@ const Login = () => {
       <Header />
       <form
         className="bg-black bg-opacity-80 fixed w-8/12 md:w-6/12 lg:w-4/12 mx-auto sm:my-36 mt-44 right-0 left-0 text-neutral-50 p-4 sm:p-12 rounded-lg"
-        onSubmit={(e) => {
+        onClick={(e) => {
           e.preventDefault();
         }}
       >
@@ -32,6 +69,7 @@ const Login = () => {
         </h1>
         {!isSignInForm && (
           <input
+            ref={name}
             type="text"
             id="mail"
             placeholder="Full Name"
